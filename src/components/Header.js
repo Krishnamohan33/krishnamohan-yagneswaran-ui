8e5eb9a07939e7d0200e8abd32bca80f52def369
Krishnamohan Yagneswaran <110534777+Krishnamohan33@users.noreply.github.com>
2025-07-31 20:49:19 +0530

    Delete src/components/Header.js

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import { krishna } from '../krishnamohan.js';
-
-export function Header({ title }) {
-  return krishna.createElement("h1", {
-    style: "font-size: 28px; color: #333; margin-bottom: 12px;"
-  }, title);
-}
